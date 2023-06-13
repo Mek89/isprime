154ca15a81ae5d3fb06e8536404f7622d5e2917e
Mek89 <mate.balogh.89@gmail.com>
2023-06-13 14:38:24 +0200

    add jsdoc

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const containerEl = document.querySelector('#container');
 
 loadWebassembly('build/release.wasm')
     .then(module => {
+        /**
+         * @param {string} value
+         * @return {number}
+         */
         const isPrimenumber = module.instance.exports.isPrimenumber;
 
         primeFormEl.addEventListener('submit', (event) => {
@@ -12,29 +16,52 @@ loadWebassembly('build/release.wasm')
                 isPrimenumber
             )(event);
 
-            containerEl.innerHTML = isPrime ? 'This is a prime' : 'This is not a prime'
+            updateContainerEl(isPrime ? 'This is a prime' : 'This is not a prime');
         });
     });
 
+/**
+ * @param {string} text
+ */
+function updateContainerEl(text) {
+    containerEl.innerHTML = text;
+}
+
+/**
+ * @param {any}
+ * @return {any}
+ */
 function pipe(...functions) {
     return args => functions.reduce((arg, fn) => fn(arg), args);
 }
 
+/**
+ * @param {Event} formEvent
+ * @return {string}
+ */
 function getFormValue(formEvent) {
     return formEvent.target['prime'].value;
 }
 
+/**
+ * @param {Event} event
+ * @return {Event}
+ */
 function preventDefault(event) {
     event.preventDefault();
     return event;
 }
 
-function loadWebassembly(filename) {
-    return fetch(filename)
-        .then(response => response.arrayBuffer())
-        .then(buffer => WebAssembly.instantiate(buffer, {
-            env: {
-                abort: () => { }
-            }
-        }));
+/**
+ * @param {string} filename
+ * @return {Promise<WebAssembly.WebAssemblyInstantiatedSource>}
+ */
+async function loadWebassembly(filename) {
+    const response = await fetch(filename);
+    const buffer = await response.arrayBuffer();
+    return await WebAssembly.instantiate(buffer, {
+        env: {
+            abort: () => { }
+        }
+    });
 }
